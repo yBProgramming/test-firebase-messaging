e7d06d25d13991307158e291b22feccd3705c473
Youathor Yiaveu <ybcelprogramming@gmail.com>
2019-07-30 09:49:02 +0700

    Test notification

---
+++ firebase/public/precache-manifest.64171834cc0e45522dfc22814841e5b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0c1b96833d5812f74c27",
+    "url": "/js/chunk-vendors.8f867765.js"
+  },
+  {
+    "revision": "97dacd893f94645a0460",
+    "url": "/js/app.047c0ac7.js"
+  },
+  {
+    "revision": "7eafe53d6b9c64e173f1",
+    "url": "/js/about.3046ed91.js"
+  },
+  {
+    "revision": "14f7c117672380b133794283b22efc3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bfe320c6ca28a57ae65ba7d12b874886",
+    "url": "/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "97dacd893f94645a0460",
+    "url": "/css/app.dde1df84.css"
+  }
+];
--- src/main.js
+++ src/main.js
@@ -3,6 +3,57 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import * as firebase from "firebase";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCB01z2ReEqTRv0jD_66TTBTgPZaxOcsco",
+  authDomain: "your-sw.firebaseapp.com",
+  databaseURL: "https://your-sw.firebaseio.com",
+  projectId: "your-sw",
+  storageBucket: "",
+  messagingSenderId: "905012060414",
+  appId: "1:905012060414:web:229b7a9dcbd007b7"
+};
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+messaging.usePublicVapidKey("BOF0N-oSAI3Q9EdEwyLxCNkf9_7lO1IYCHvFsNq-Nm4JhZ_71CLV9x7Q2VginiZrn5OC8d6Nww9eLtx7sQ1K8xc"); // 1. Generate a new key pair
+// Request Permission of Notifications
+messaging.requestPermission().then(() => {
+  console.log('Notification permission granted.');
+
+  // Get Token
+  messaging.getToken().then((token) => {
+    console.log(token)
+    prompt("Notification Token", token);
+  })
+}).catch((err) => {
+  console.log('Unable to get permission to notify.', err);
+});
+
+messaging.onMessage(function(playload){
+  console.log(playload['notification']['title'])
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+  else if (Notification.permission === "granted") {
+    var options = {
+        body: playload['notification']['body']
+    };
+    var n = new Notification(playload['notification']['title'], options);
+  }
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var options = {
+            body: playload['notification']['body']
+        };
+        var n = new Notification(playload['notification']['title'], options);
+      }
+    });
+  }
+})
 
 Vue.config.productionTip = false
 
@@ -11,3 +62,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
